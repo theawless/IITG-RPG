b1a55dd964f6bd8db6a1258bd9ca438e9b918de6
Abhinav Singh <theawless@gmail.com>
2017-01-27 00:44:18 +0530

    Rewrote Singleton
    We do not need a gameobject per class,
    a component will suffice.

---
+++ Assets/Script/Manager/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Script.Manager
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static readonly object lockObject = new object();
+        private static readonly GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
+        private static T instance = gameController.AddComponent<T>();
+
+        public static T Instance
+        {
+            get { return instance; }
+        }
+
+        private void OnApplicationQuit()
+        {
+            instance = null;
+        }
+    }
+}
