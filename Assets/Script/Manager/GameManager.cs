918b364d8857e456f1e88ae8881ee6373caec2c8
Abhinav Singh <theawless@gmail.com>
2017-01-27 00:49:13 +0530

    Fixed namespace for GameManager

---
+++ Assets/Script/Manager/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.Manager
+{
+    //TODO : Incomplete
+    public class GameManager : Singleton<GameManager>
+    {
+        private GameManager() { }
+
+        public static Dictionary<string, int> save;
+        public enum State { State1, State2 };
+        public enum Flags { bool1, bool2 };
+
+        List<bool> flags;
+        public State GameState { get; set; }
+
+        void Start()
+        {
+            GameState = State.State1;
+            flags = new List<bool>(new bool[2]);
+        }
+
+        void Add(string vari, int val)
+        {
+            save.Add(vari, val);
+        }
+
+        void ChangeState(Flags trig)
+        {
+            switch (GameState)
+            {
+                case State.State1: Debug.Log("1"); break;
+                case State.State2: Debug.Log("2"); break;
+            }
+        }
+    }
+}
